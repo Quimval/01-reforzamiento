a307342bdd95c78342307c4f7d84d44e0317badd
Mrnachoman11 <cristian.vega@quimval.cl>
2022-04-05 16:21:34 -0400

    Logout con timer
    Logout después de 1,5 segundos

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,76 @@
-import React from 'react'
+import { type } from '@testing-library/user-event/dist/type';
+import { stat } from 'fs';
+import React, { useEffect, useReducer } from 'react'
+
+
+interface AuthState {
+    validando: boolean;
+    token: string | null;
+    username: string;
+    nombre: string;
+
+}
+
+const initalState: AuthState = {
+    validando: true,
+    token: null,
+    username: '',
+    nombre: ''
+}
+
+type AuthAction = { type: 'logout' };
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+
+    switch (action.type) {
+        case 'logout':
+            return {
+                validando: false,
+                token: null,
+                nombre: '',
+                username: ''
+            }
+
+            break;
+
+        default:
+            return state;
+    }
+
+}
 
 export const Login = () => {
+
+    const [{validando}, dispatch] = useReducer(authReducer, initalState);
+
+    useEffect(() => {
+        setTimeout(() => {
+            dispatch({ type: 'logout' });
+        }, 1500);
+    }, []);
+
+    if (validando) {
+        return (
+            <>            <h3>Login</h3>
+
+                <div className='alert alert-info'>Validando...</div></>
+        )
+    }
+
+
     return (
         <>
             <h3>Login</h3>
 
-            <div className='alert alert-info'>Validando...</div>
             <div className='alert alert-danger'>No autenticado</div>
             <div className='alert alert-success'>Autenticado</div>
 
             <button className='btn btn-primary'>
                 Login
+            </button>
+
+            <button className='btn btn-danger'>
+                Log Out
             </button>
         </>
     )
